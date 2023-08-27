8c4a892c4a3eb8fa4189f0e15a179ca5ff2f731d
laser-wound <137529791+laser-wound@users.noreply.github.com>
2023-08-27 04:30:44 +0400

    Initial Commit

---
+++ Tabby Cat/Tabby_CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_CatApp.swift
+//  Tabby Cat
+//
+//  Created by Omar Alshehhi on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
